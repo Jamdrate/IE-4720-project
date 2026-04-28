cd05ae8e00fe922d33f51670ccdd1af109f7773f
Adomale Kiobel <kennyadam14@gmail.com>
2026-04-28 13:52:28 -0500

    Made some changes

---
+++ isubookstore/app/books/page.tsx
@@ -0,0 +1,8 @@
+export default function BooksPage() {
+  return (
+    <section>
+      <h1>Books</h1>
+      <p>Browse available textbooks and bookstore titles.</p>
+    </section>
+  );
+}
---
+++ isubookstore/app/cart/page.tsx
@@ -0,0 +1,8 @@
+export default function CartPage() {
+  return (
+    <section>
+      <h1>Cart</h1>
+      <p>Your selected books will appear here.</p>
+    </section>
+  );
+}
